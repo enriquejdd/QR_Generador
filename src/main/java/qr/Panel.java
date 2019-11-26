7d7b0b1da208d5b5e95734ec77596d858fc2b1ff
enrique <enriquejdd1999@gmail.com>
2019-11-26 12:25:31 +0100

    Añade clase Panel

---
+++ src/main/java/qr/Panel.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package qr;
+
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author usuario
+ */
+public class Panel extends JPanel{
+    
+    // Tamaño de la ventana. Constantes
+    public static final int ANCHO_FRAME = 600;
+    public static final int ALTO_FRAME = 200;
+    
+}
