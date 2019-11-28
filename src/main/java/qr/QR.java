8d017ce08aee01061b590f5a84a22eb51019adee
enrique <enriquejdd1999@gmail.com>
2019-11-28 11:01:51 +0100

    Añade clase QR

---
+++ src/main/java/qr/QR.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package qr;
+
+/**
+ *
+ * @author usuario
+ */
+public class QR {
+    // Atributos de clase
+    public static final int ANCHO_QR = 400;
+    public static final int ALTO_QR = 400;
+    
+    
+}
